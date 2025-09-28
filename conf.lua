bc4ef79fe8108d2725b523782ff45e8fa313bbff
whataremark <markbsmn@gmail.com>
2025-09-28 23:54:12 +0200

    ff kleiine commit

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.title  = "Zweeds Pesten"
     t.window.width  = 1920  
     t.window.height = 1080
-    t.console       = false   -- handig voor debug-prints
+    t.console       = true   -- handig voor debug-prints
     t.window.resizable = true -- maak venster aanpasbaar
     t.window.icon = "assets/icon.png" -- zet het icoon van het venster
 end
